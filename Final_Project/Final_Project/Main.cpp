8d8e0f7edd962999858c9268b1537c8bd36b866c
Bành Hảo Toàn <86637323+mjkundta@users.noreply.github.com>
2022-04-12 17:06:46 +0700

    test

--- Final_Project/Final_Project/Main.cpp
+++ Final_Project/Final_Project/Main.cpp
@@ -1,15 +1,10 @@
 #include "ConsoleGUI.h"
 #include "Login.h"
 #include "Graphics.h"
-#include "Menu.h"
 int main()
 {
 	FixConsoleWindow();
-	//cout << "Group 12A" << endl;
-	//DrawText("_assets//login_bg.txt", 25, 5);
 	User dthgiang;
 	LoginLoop(dthgiang);
-	//DrawStaffMenu();
-	
 	return 0;
 }
