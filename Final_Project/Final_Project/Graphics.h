7a82a28b2d14e9d2c18c58078e387db5e7fdaa1b
Bành Hảo Toàn <86637323+mjkundta@users.noreply.github.com>
2022-04-12 17:06:21 +0700

    xoa comment

--- Final_Project/Final_Project/Graphics.h
+++ Final_Project/Final_Project/Graphics.h
@@ -13,28 +13,8 @@
 
 using namespace std;
 
-/*
-Chú thích cho hàm Set Color:
-        0 = Black       8 = Gray
-        1 = Blue        9 = Light Blue
-        2 = Green       10 = Light Green
-        3 = Aqua        11 = Light Aqua
-        4 = Red         12 = Light Red
-        5 = Purple      13 = Light Purple
-        6 = Yellow      14 = Light Yellow
-        7 = White       15 = Bright White
-
-        Sử dụng công thức i * 16 + j để tạo ra màu mới với
-        i là mã màu nền, j là mã màu chữ
-        Ví dụ: 2 = 0 * 16 + 2 suy ra wAttributes = 2
-        thì màu nền là Black, màu chữ là Green;
-        80 = 5 * 16 + 0 suy ra wAttributes = 80
-        thì màu nền là Purple, màu chữ là Black.
-
-*/
-
 struct Mark {
-	string ID;// linh hoạt trong điểm của sinh viên thì là id môn học, còn trong môn học thì là id sinh viên
+	string ID;
 	string Name;
 	string C;
 	double Total_Mark;
@@ -46,19 +26,16 @@ struct MarkNode {
 	Mark data;
 	MarkNode* pNext;
 };
-//struct thông tin học sinh hoặc giáo viên, nếu là giáo viên thì 1 số thông tin bỏ trống
 struct Data {
 	int NO_inclass;
 	string IDstd;
 	string name;
-	string sex;// 0 là nam, 1 là nữ.
-	string Bir;// ngày sinh
+	string sex;
+	string Bir;
 	string IDsocial;
 	string Class;
-	MarkNode* phead;// nhiều môn
+	MarkNode* phead;
 };
-//
-// struct 1 khóa học, 1 số thông tin có thể trống
 struct Course {
 	string ID_course;
 	string name;
